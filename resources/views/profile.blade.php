7977270334f3652e6ce047c0a9dd7b98f677ab69
Luthfi Aditya <luthfi2004aditya@gmail.com>
2024-09-06 19:23:42 +0700

    Modifying Landing Page of Laravel 10

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,24 @@
+<h1>ini adalah halaman profile</h1>
+<p>
+    Nama    : Luthfi Aditya <br>
+    Kelas   : D <br>
+    NPM     : 2217051062
+</p>
+
+<table>
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td>{{ $nama }}</td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>:</td>
+        <td>{{ $kelas }}</td>
+    </tr>
+    <tr>
+        <td>NPM</td>
+        <td>:</td>
+        <td>{{ $npm }}</td>
+    </tr>
+</table>
