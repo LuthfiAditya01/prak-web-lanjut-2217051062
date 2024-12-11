31d9597f5aa4b76f6e607315acdfe029a2073391
LuthfiAditya01 <luthfi2004aditya@gmail.com>
2024-12-12 06:29:05 +0700

    Adding Index List Pages

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.app')
 @section('content')
-<a href="{{ route('user_create') }}" class="btn btn-primary mb-3">Tambah Pengguna Baru</a>
+<body class="bg-cover bg-no-repeat" style="background-image: url('{{ asset('jjkwp.jpg') }}')">
+    <div class="m-72 bg-[#ffa68c] rounded-xl p-5 bg-opacity-75">
+        <a href="{{ route('user_create') }}" class="mb-3 p-2 bg-[#d34e63] rounded-xl font-semibold transition-all ease-in-out bg-opacity-100 hover:p-3 hover:bg-opacity-40">Tambah Pengguna Baru</a><br><br>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nama</th>
-            <th>NPM</th>
-            <th>Kelas</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-            foreach ($users as $user) {
-        ?>
-        <tr>
-            <td><?= $user['id'] ?></td>
-            <td><?= $user['nama'] ?></td>
-            <td><?= $user['npm'] ?></td>
-            <td><?= $user['nama_kelas'] ?></td>
-            <td></td>
-        </tr>
-        <?php
-            }
-        ?>
-    </tbody>
-</table>
+        <table class="table-fixed w-full border border-black">
+            <thead class="caption-top">
+                <tr>
+                    <th class="border border-black">ID</th>
+                    <th class="border border-black">Nama</th>
+                    <th class="border border-black">NPM</th>
+                    <th class="border border-black">Kelas</th>
+                    <th class="border border-black">Aksi</th>
+                </tr>
+            </thead>
+            <center><tbody class="text-center">
+                <?php
+                foreach ($users as $user) {
+                ?>
+                    <tr>
+                    <td class="hover:bg-[#d34e63] hover:font-bold ease-in-out duration-500 border border-black"><?= $user['id'] ?></td>
+                    <td class="hover:bg-[#d34e63] hover:font-bold ease-in-out duration-500  border border-black"><?= $user['nama'] ?></td>
+                    <td class="hover:bg-[#d34e63] hover:font-bold ease-in-out duration-500  border border-black"><?= $user['npm'] ?></td>
+                    <td class="hover:bg-[#d34e63] hover:font-bold ease-in-out duration-500  border border-black"><?= $user['nama_kelas'] ?></td>
+                    <td class="hover:bg-[#d34e63] hover:font-bold ease-in-out duration-500 border border-black"></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody></center>
+        </table>
+    </div>
+</body>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 Route::get('/user/profile', [UserController::class, 'profile']) -> name('user_profile');  
 Route::get('/user/create', [UserController::class, 'create']) -> name('user_create');  
 Route::post('/user/store', [UserController::class,'store']) -> name('user_store');
-Route::get('/user/index', [UserController::class, 'index'])->name('user_index');
+Route::get('/user', [UserController::class, 'index'])->name('user_index');
 
 // Route::put('/user/{id}', [UserController::class, 'update'])->name(user.udpate)
 // Route::get('/user/delete', [UserController::class, 'edit']) -> name(user.edit);
